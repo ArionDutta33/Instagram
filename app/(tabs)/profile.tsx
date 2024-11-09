6214cfbd16df515877183403f5ce257e906f93e4
ArionDutta33 <ariondutta333@outlook.com>
2024-11-09 14:56:00 +0530

    Supabase auth

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,11 +1,11 @@
-import { View, Text, Image, TextInput } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, Image, TextInput } from 'react-native';
+import React, { useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
 import Button from '~/components/Button';
 
 const ProfileScreen = () => {
-  const [image, setImage] = useState<string | null>(null)
-  const [username, setUsername] = useState<string>('')
+  const [image, setImage] = useState<string | null>(null);
+  const [username, setUsername] = useState<string>('');
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -22,25 +22,35 @@ const ProfileScreen = () => {
     }
   };
   return (
-    <View className='p-3 flex-1'>
+    <View className="flex-1 p-3">
       {/* Avatar image picker */}
-      {image ? (<Image className='rounded-full bg-slate-400  w-52 aspect-square self-center' source={{ uri: image }} />
+      {image ? (
+        <Image
+          className="aspect-square w-52  self-center rounded-full bg-slate-400"
+          source={{ uri: image }}
+        />
       ) : (
-        <View className='bg-slate-400 rounded-full w-52 aspect-square self-center' />
+        <View className="aspect-square w-52 self-center rounded-full bg-slate-400" />
       )}
-      <Text onPress={pickImage} className='self-center text-blue-500 font-semibold m-5'>Change</Text>
+      <Text onPress={pickImage} className="m-5 self-center font-semibold text-blue-500">
+        Change
+      </Text>
 
       {/* form */}
-      <Text className='mb-2 text-gray-500 font-semibold'>Username</Text>
-      <TextInput placeholder='Username' value={username} onChangeText={setUsername} className='border border-gray-300 rounded-md p-3 ' />
+      <Text className="mb-2 font-semibold text-gray-500">Username</Text>
+      <TextInput
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+        className="rounded-md border border-gray-300 p-3 "
+      />
       {/* button */}
-      <View className='gap-2 mt-auto'>
-        <Button title='Update Profile' />
-        <Button title='Sign out ' />
+      <View className="mt-auto gap-2">
+        <Button title="Update Profile" />
+        <Button title="Sign out " />
       </View>
-
     </View>
-  )
-}
+  );
+};
 
-export default ProfileScreen
+export default ProfileScreen;
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,8 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Redirect } from 'expo-router'
+import { View, Text } from 'react-native';
+import React from 'react';
+import { Redirect } from 'expo-router';
 const Home = () => {
-  return (
-   <Redirect href="/(tabs)" />
-  )
-}
+  return <Redirect href="/(auth)" />;
+};
 
-export default Home
+export default Home;
