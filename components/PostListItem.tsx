e588d00e1d198c92c20ad426b235c0d233b69a34
ArionDutta33 <ariondutta333@outlook.com>
2024-11-09 12:42:34 +0530

    Cloudinary config

--- components/PostListItem.tsx
+++ components/PostListItem.tsx
@@ -1,18 +1,36 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, useWindowDimensions } from 'react-native'
 import React from 'react'
 import { AntDesign } from '@expo/vector-icons'
 import { Ionicons } from '@expo/vector-icons'
 import { Feather } from '@expo/vector-icons'
+import { AdvancedImage } from 'cloudinary-react-native';
+
+
+// Import required actions and qualifiers.
+import { thumbnail } from "@cloudinary/url-gen/actions/resize";
+import { focusOn } from "@cloudinary/url-gen/qualifiers/gravity";
+import { FocusOn } from "@cloudinary/url-gen/qualifiers/focusOn";
+import { cld } from '~/libs/cloudinary'
 
 const PostListItem = ({ post }) => {
+    // cld.image returns a CloudinaryImage with the configuration set.
+    const image = cld.image(post.image);
+    const { width } = useWindowDimensions()
+    // Apply the transformation.
+    image
+        .resize(thumbnail().width(width).height(width))  // Crop the image, focusing on the face.
+
+
+    const avatar = cld.image(post.user.avatar_url)
+    avatar.resize(thumbnail().width(48).height(48).gravity(focusOn(FocusOn.face())))
 
     return (
         <View className='bg-white'>
             <View className='p-3 flex-row items-center gap-2'>
-                <Image className='w-12 aspect-square rounded-full' source={{ uri: post.user.image_url }} />
+                <AdvancedImage className='w-12 aspect-square rounded-full' cldImg={avatar} />
                 <Text className='font-semibold '>{post.user.username}</Text>
             </View>
-            <Image className='w-full aspect-[4/3]' source={{ uri: post.image_url }} />
+            <AdvancedImage cldImg={image} className='w-full aspect-[4/3]' />
             <View className='flex-row gap-3 p-3'>
                 <AntDesign name="hearto" size={20} color="black" />
                 <Ionicons name="chatbubble-outline" size={20} color="black" />
