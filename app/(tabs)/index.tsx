fd7f88d8c8e130559ff2d9bc44f819bc6311a4e7
ArionDutta33 <ariondutta333@outlook.com>
2024-11-15 13:20:52 +0530

    refresh control

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, FlatList, Alert } from 'react-native';
+import { View, Text, Image, FlatList, Alert, RefreshControl } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import posts from '../../assets/data/posts.json';
 import PostListItem from '~/components/PostListItem';
@@ -6,16 +6,19 @@ import { supabase } from '~/utils/supabase';
 
 const FeedScreen = () => {
   const [posts, setPosts] = useState([]);
+  const [isRefreshing, setIsRefreshing] = useState(false); // State to manage refreshing
   useEffect(() => {
     fetchPosts();
   });
 
   const fetchPosts = async () => {
+    setIsRefreshing(true); // Start refreshing
     let { data, error } = await supabase.from('posts').select('*,user:profiles(*)');
     if (error) {
       Alert.alert(error.message);
     }
     setPosts(data);
+    setIsRefreshing(false); // Stop refreshing
   };
   console.log(posts);
 
@@ -26,6 +29,13 @@ const FeedScreen = () => {
         contentContainerClassName="items-center "
         contentContainerStyle={{ gap: 10, maxWidth: 512, width: '100%' }}
         showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl
+            refreshing={isRefreshing}
+            onRefresh={fetchPosts} // Trigger fetchPosts on pull-to-refresh
+            colors={['#9Bd35A', '#689F38']} // Optional: Customize spinner color
+          />
+        }
         renderItem={({ item }) => <PostListItem post={item} />}
       />
     </View>
